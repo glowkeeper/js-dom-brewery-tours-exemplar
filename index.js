a746cbcc9796280efef250fde99e33a638af643a
steve <steve.huckle@gmail.com>
2022-07-10 18:35:21 +0100

    fetch all data

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const filterByName = document.querySelector('#search-breweries')
 const filterByCityForm = document.querySelector('#filter-by-city-form')
 const filterByCityClearBtn = document.querySelector('.clear-all-btn')
 
+const nextPage = document.querySelector("#next")
+const prevPage = document.querySelector("#prev")
+const pageNumber = document.querySelector("#page-number")
+
 const url = 'https://api.openbrewerydb.org/breweries'
 const perPage = 50
 
@@ -19,7 +23,10 @@ const visit = 'Visit Website'
 const state = {
   types: [],
   breweries: {},
+  page: 1,
+  hasFetched: false,
   filters: {
+    USState: '',
     type: '',
     names: {
       key: 'name',
@@ -32,67 +39,14 @@ const state = {
   }
 }
 
-function render() {
-  renderTypeOptions()
-  renderBreweries()
-  renderCityFilters()
-}
 
-function renderTypeOptions() {
-  filterByType.innerHTML = ''
-  const selectType =  document.createElement('option')
-  selectType.innerText = 'Select a type...'
-  filterByType.append(selectType)
-  state.types.forEach(type => {
-    const newType = document.createElement('option')
-    newType.value = type
-    newType.innerText = type
-
-    filterByType.append(newType)
-  })
-}
-
-function renderBreweries() {
-  breweriesList.innerHTML = ''
-  const breweries = applyFilters()
-  breweries.forEach(brewery => {
-    renderBreweryCard(brewery)
-  })
-}
-
-function renderCityFilters() {
-  const breweries = applyFilters()
-  const cities = [...new Set(breweries.map(brewery => brewery.city))] // unique cities
-
-  filterByCityForm.innerHTML = ''
-  cities.forEach(city => {
-    const input = document.createElement('input')
-    input.setAttribute('type', 'checkbox')
-    input.setAttribute('name', city)
-    input.setAttribute('value', city)
-
-    const label = document.createElement('label')
-    label.setAttribute('for', city)
-    label.innerText = city
-
-    input.addEventListener('change', event => {
-      if (event.target.checked) {
-        state.filters.cities.values.push(city)
-      } else {
-        const newCities = state.filters.cities.filter(filterCity => filterCity !== city)
-        state.filters.cities.values = newCities
-      }
-
-      renderBreweries()
-    })
-
-    filterByCityForm.append(input, label)
-  })
-}
 
 function doFiltering ( filterKey, breweryKey, breweries ) {
+
+  //console.log('doing', filterKey, breweryKey, breweries)
   
   if (state.filters[filterKey].values.length) {
+    
     breweries = breweries.filter(brewery => {
       const breweryValue = brewery[breweryKey].toLowerCase();
       return state.filters[filterKey].values.find((searchTerm) => {
@@ -114,13 +68,92 @@ function applyFilters() {
     breweries = Object.keys(state.breweries).map(breweryType => state.breweries[breweryType]).flat()
   }
 
+  if (state.filters.USState) {
+    breweries = breweries.filter(brewery => {
+      const thisState = brewery.state ? brewery.state.toLowerCase() : ''
+      return thisState === state.filters.USState.toLowerCase()
+    })
+  }
+
   Object.keys(state.filters).forEach(filter => {
-    if (filter !== 'type') { // type is a special case, and we've already filtered for that
+    if (filter !== 'type' && filter !== 'USState') { // type and state are special cases
       // all other filters are of the same form (key name, array of terms we need to search for), so we can use doFiltering to help us
       breweries = doFiltering(filter, state.filters[filter].key, breweries)
     }
   })
 
+  return breweries
+}
+
+function renderTypes() {
+  filterByType.innerHTML = ''
+  const selectType =  document.createElement('option')
+  selectType.innerText = 'Select a type...'
+  filterByType.append(selectType)
+  state.types.forEach(type => {
+    const newType = document.createElement('option')
+    newType.value = type
+    newType.innerText = type
+
+    filterByType.append(newType)
+  })
+}
+
+function renderCities(breweries) {
+  const cities = [...new Set(breweries.map(brewery => brewery.city))].sort() // unique cities
+
+  filterByCityForm.innerHTML = ''
+  cities.forEach(city => {
+    const input = document.createElement('input')
+    input.setAttribute('type', 'checkbox')
+    input.setAttribute('name', city)
+    input.setAttribute('value', city)
+
+    const label = document.createElement('label')
+    label.setAttribute('for', city)
+    label.innerText = city
+
+    input.addEventListener('change', event => {
+      if (event.target.checked) {
+        state.filters.cities.values.push(city)
+      } else {
+        const newCities = state.filters.cities.values.filter(filterCity => filterCity !== city)
+        state.filters.cities.values = newCities
+      }
+
+      render()
+    })
+
+    filterByCityForm.append(input, label)
+  })
+}
+
+function renderBreweries(breweries) {
+  breweriesList.innerHTML = ''
+  breweries.forEach(brewery => {
+    renderBreweryCard(brewery)
+  })
+}
+
+function renderPaging(breweries) {
+  
+  const pageEnd = Math.ceil(breweries.length / perPage)
+
+  pageNumber.innerText = `Viewing ${state.page} of ${pageEnd}`
+  
+  const start = (state.page - 1) * perPage
+  let end = (state.page) * perPage
+  
+  if ( end >= breweries.length) {
+    end = breweries.length
+    nextPage.disabled = true
+  } else {
+    nextPage.disabled = false
+  }
+  start === 0 ? prevPage.disabled = true : prevPage.disabled = false
+  
+  console.log('end', state.page, end, breweries.length, start)
+  breweries = breweries.slice(start, end)
   return breweries
 }
 
@@ -164,28 +197,35 @@ function renderBreweryCard(brewery) {
   breweriesList.append(li)
 }
 
+function render(needCitiesList = false) {
+  if(state.hasFetched) {
+    let breweries = applyFilters()
+    renderTypes()
+    if(needCitiesList) {
+      renderCities(breweries)
+    }
+    breweries = renderPaging(breweries)
+    renderBreweries(breweries)
+  }
+}
+
 const addListeners = () => {
 
   // form submit
   stateForm.addEventListener('submit', event => {
     event.preventDefault()
-    const byState = event.target[0].value
-    fetch(`${url}?by_state=${byState}&per_page=${perPage}`)
-      .then(res => res.json())
-      .then(data => {
+    state.filters.USState = event.target[0].value
 
-        state.types = [...new Set(data.map(brewery => brewery.brewery_type))] // unique types
-        state.types.forEach(type => {
-          state.breweries[type] = data.filter(brewery => brewery.brewery_type === type)
-        })
+    state.page = 1
 
-        render()
-      })
+    const needCitiesList = true
+    render(needCitiesList)
   })
 
   // drop down select options
   filterByType.addEventListener('change', event => {
     state.filters.type = event.target.value
+    state.page = 1
 
     render()
   })
@@ -193,6 +233,7 @@ const addListeners = () => {
   // text input
   filterByName.addEventListener('input', event => {
     state.filters.names.values[0] = event.target.value
+    state.page = 1
 
     render()
   })
@@ -201,10 +242,76 @@ const addListeners = () => {
   filterByCityClearBtn.addEventListener('click', event => {
     state.filters.cities.value = []
     filterByCityForm.reset()
+    state.page = 1
 
+    render()
+  })
+
+  nextPage.addEventListener("click", function () {
+    state.page++
+
+    render()
+  })
+
+  prevPage.addEventListener("click", function () {
+    state.page--
+    
     render()
   })
 
 }
 
+const doFetch = async (url, pageNumber) => {
+
+  const thisURL = `${url}?page=${pageNumber}&per_page=${perPage}`
+  //console.log('url', thisURL)
+  const result = await fetch(thisURL)
+  const data = await result.json()
+  return data
+}
+
+const typeFetchedData = (breweries) => {
+  //console.log('all', breweries)
+
+  state.types = [...new Set(breweries.map(brewery => brewery.brewery_type))] // unique types
+  state.types.forEach(type => {
+    state.breweries[type] = breweries.filter(brewery => brewery.brewery_type === type)
+  })
+
+  //console.log('types', state.types)
+  //console.log('data', state.breweries)
+
+  const needCitiesList = true
+  render(needCitiesList)
+}
+
+const fetchData = async () => {
+  let pageNumber = 1
+  let fetchData = []
+  let allData = []
+  let firstFetch = true
+
+  do {
+
+    if ( state.hasFetched ) {
+      allData = [...allData, fetchData].flat()
+      if(firstFetch) {
+        firstFetch = false
+        typeFetchedData(allData)
+      }
+    } else { 
+      state.hasFetched = true
+    }
+
+    pageNumber++
+    fetchData = await doFetch(url, pageNumber)    
+    //console.log('fetched', fetchData, pageNumber)
+
+  } while (fetchData.length);
+  
+  //console.log('all', allData)
+  typeFetchedData(allData)
+}
+
 addListeners()
+fetchData()
